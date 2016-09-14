0d924e8e7dedac57b20aca094b7c01ce57d2088c
Divya Sampath Kumar <disa6302@colorado.edu>
2016-09-14 02:37:02 -0600

    Call to reverse()

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
 #include<stdio.h>
-void main()
+#include "hw1.h"
+int main()
 {
-	printf("Hello World\n");
+	int length=0,i=0;
+	char arr[30];
+
+	printf("\nEnter the string:");
+	scanf("%[^\n]s",arr);
+	while(arr[i]!='\0')
+	{
+		length++;
+		i++;
+	}
+	printf("\nThe original string is:");
+	for(i=0;i <=length;i++)
+	{
+		printf("%c",arr[i]);
+	}
+
+	char a=reverse(arr,length);
+	if(a!=0)
+		printf("\nError in Input!!");
+	else
+	{
+	printf("\nThe reversed string is:");
+	for(i=0;i<=length;i++)
+	{
+		printf("%c",arr[i]);
+	}
+	}
+	printf("\n");
+
 }
